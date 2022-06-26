c5636c2891a16eefd63fd06819b8dad4c8fea1ef
roofrack <robaylard@gmail.com>
2022-06-26 15:34:50 +0000

    june 25

--- javascript/play/callback.js
+++ javascript/play/callback.js
@@ -8,8 +8,8 @@ function getUsers(callback) {
 }
 
 function findUser(username, callback) {
-  getUsers((users) => {
-    const user = users.find((user) => user.username === username);
+    getUsers((users) => {
+      const user = users.find((user) => user.username === username)
     callback(user);
   });
 }
@@ -28,9 +28,10 @@ Then the array of data gets passed into this callback function
 as an argument. It gets stuffed in and replaces the 'users'
 parameter. Kapeeesh?
 
-Also I was confused about how the find((user) => user.username === username)) callback worked.
+Also I was confused about how the 'find((user) => user.username === username))' callback worked.
 What does the user in find(user) refer to? But its just a placeholder. It is a parameter and all
 its doing is saying find in that array above the object where the username is equal to "john".
+Just accept this Robert. Can use any word here instead of user. Chill man.
 
 Do not be confused by the callback parameter in the findUser function.
 That 'callback' parameter gets replaced by 'console.log'. Very simple.
---
+++ javascript/play/find.js
@@ -0,0 +1,8 @@
+// Using find()
+// from mdn find
+
+const array1 = [5, 12, 8, 130, 44];
+
+const found = array1.find(element => element > 10);
+
+console.log(found)
